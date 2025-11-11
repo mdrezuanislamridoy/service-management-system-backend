42a226862bf0e4056be1b1e55f05358ce9fecfca
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-11-12 00:20:58 +0600

    fixed cookie parser issue

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import handleError from "./middlewares/handleError.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import { authRouter } from "./modules/auth/auth.routes.js";
 import { serviceRouter } from "./modules/services/service.routes.js";
 import { bookingRouter } from "./modules/booking/booking.routes.js";
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/services", serviceRouter);
